447e73dd7f8831d17c07b4afe2a6f773c6b41311
amkrtchy <anna_mkrtchyan@sfu.ca>
2017-12-01 11:13:25 -0800

    Bug fixes in cnn and mlp

--- mlp.py
+++ mlp.py
@@ -1,5 +1,12 @@
+#import plaidml.keras
+#plaidml.keras.install_backend()
+
 import pandas as pd
 import numpy as np
+from sklearn.metrics import log_loss
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from collections import OrderedDict
 from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Input, Activation, LSTM
 from keras.layers import Conv1D, MaxPooling1D, GlobalMaxPooling1D
@@ -22,18 +29,11 @@ def create_average_vec(doc, text_dim, word2vec_model):
 
 def main():
 
-	data_dir = '../data/'
-	train = pd.read_csv(data_dir + 'train.csv')
-	test = pd.read_csv(data_dir + 'test.csv')
+	text_dim = 200
 
 	mlp_train, mlp_test= read_and_select(['id', 'text'])
 	w2v_model = w2v(mlp_train, mlp_test)
 	w2v_model.train_model()
-
-	layers = [2, 3, 5] # number of layers
-	learning_rates = [0.005, 0.01, 0.05, 0.1] # learning rate
-	units = [200, 300, 500] # number of units per layer
-	dropout = [0, 0.2, 0.5] # use dropout or no? what's dropout rate?
 
 	proc_train = mlp_train['processed_text']
 	proc_test = mlp_test['processed_text']
@@ -47,14 +47,18 @@ def main():
 	for i in range(len(proc_test)):
 		test_vec[i] = create_average_vec(proc_test[i], text_dim, w2v_model)
 
-	# Transform labels into one hot encoded format.
-	y_train_ohe = label_binarize(mlp_train['author'], classes=['EAP', 'HPL', 'MWS'])
 
+	y_train_ohe = label_binarize(mlp_train['author'], classes=['EAP', 'HPL', 'MWS'])
 	X_train, X_val, y_train, y_val = train_test_split(train_vec, y_train_ohe, test_size=0.2, random_state=21)
 	X_test = test_vec
 
-    # Define number of epochs
-	epochs = 30
+  
+	layers = [2, 3] # number of layers
+	learning_rates = [0.005, 0.01, 0.05] # learning rate
+	units = [200, 300, 500] # number of units per layer
+	dropout = [0.2, 0.5] # use dropout or no? what's dropout rate?
+	epochs = 50
+
 	all_scores = dict()
 	errors = dict()
 
